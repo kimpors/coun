337a1a3a443c42299505d6d74e8d4b11b3823fab
kimpors <mikita_korshak@ukr.net>
2025-06-17 21:23:14 +0300

    Add basic functions
    for counting chars, words, lines
    
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

--- src/main.c
+++ src/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <ctype.h>
+
+size_t couword(char *s);
+size_t couline(char *s);
+size_t couchar(char *s);
 
 int main(void)
 {
-	printf("hello there\n");
+	char *s = "\nhello there\n";
+
+	printf("len: %ld\n", couchar(s));
+	printf("words: %ld\n", couword(s));
+	printf("lines: %ld\n", couline(s));
 	return 0;
+}
+
+// count chars
+size_t couchar(char *s)
+{
+	char *ps = s;
+	while (*ps++);
+	return ps - s;
+}
+
+// count words
+size_t couword(char *s)
+{
+	size_t w = 0;
+
+	while (*s && !isalpha(*s)) s++;
+
+	while (*s)
+	{
+		while (*s && isalpha(*s)) s++;
+		if (*s) w++;
+		while (*s && !isalpha(*s)) s++;
+	}
+
+	return w;
+}
+
+// count lines
+size_t couline(char *s)
+{
+	size_t l = 0;
+
+	while (*s)
+	{
+		if (*s++ == '\n') l++;
+	}
+
+	return l;
 }
